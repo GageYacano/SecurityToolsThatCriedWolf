48a86d6019bbc4ed17afe36f738a153312aae72b
Mal <133685339+maloudnee@users.noreply.github.com>
2026-04-07 08:32:33 -0400

    Application Layer Logic

--- onionmanager/src/main/java/com/seniordesign/Apps.java
+++ onionmanager/src/main/java/com/seniordesign/Apps.java
@@ -1,5 +1,11 @@
 package com.seniordesign;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class Apps implements LayerRequirements{
 	
@@ -9,13 +15,53 @@ public class Apps implements LayerRequirements{
 	
 	// Gets all the data you will be needing. This is basically your main
 	public void loadData(){
-		String myData = "";
-		// Get data, blah blah blah
+		StringBuilder myData = new StringBuilder();
+		ObjectMapper mapper = new ObjectMapper();
 		
-		
-		if(checkDuplicate()){
-			data = myData;
+		try {
+			ProcessBuilder pb = new ProcessBuilder(
+				"system_profiler",
+				"SPApplicationsDataType",
+				"-detailLevel",
+				"mini",
+				"-json"
+			);
+			pb.redirectErrorStream(true);
+			Process process = pb.start();
+
+			try (BufferedReader reader = new BufferedReader(
+					new InputStreamReader(process.getInputStream()))) {
+				String line;
+				while ((line = reader.readLine()) != null) {
+					myData.append(line).append("\n");
+				}
+			}
+			process.waitFor();
+
+			JsonNode root = mapper.readTree(myData.toString());
+			JsonNode appList = root.path("SPApplicationsDataType");
+			ArrayNode filteredApps = mapper.createArrayNode();
+			
+			if (appList.isArray()) {
+				for (JsonNode app : appList) {
+					ObjectNode appEntry = mapper.createObjectNode();
+					appEntry.put("name", app.path("_name").asText());
+					appEntry.put("version", app.path("version").asText());
+					appEntry.put("path", app.path("path").asText());
+					filteredApps.add(appEntry);
+				}
+			}
+
+			ObjectNode result = mapper.createObjectNode();
+			result.set("applications", filteredApps);
+
+			this.data = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(result);
+            System.out.println(this.data);
+			
+		} catch (IOException | InterruptedException e) {
+			e.printStackTrace();
 		}
+		
 	}
 	
 	
